521ff631d4bf0dcfcd3a1b22077299f2ea61d6e8
dongyu <dongyu@ubinavi.com.cn>
2024-03-04 11:07:21 +0800

    feat: init blog

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'yu', // Site author
+	title: 'yu blog', // Site title.
+	description: 'records and technology', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
